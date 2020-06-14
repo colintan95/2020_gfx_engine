a9c13b6ef771f797d6042e0df7d79970097c655b
Colin Tan <colintan95@gmail.com>
2020-06-13 21:43:09 -0700

    Begin: Create scene and render system.

---
+++ src/render/render.h
@@ -0,0 +1,12 @@
+#ifndef RENDER_RENDER_H_
+#define RENDER_RENDER_H_
+
+namespace render {
+
+class Model {
+
+};
+
+} // namespace
+
+#endif // RENDER_RENDER_H_
---
+++ src/scene/scene.h
@@ -0,0 +1,12 @@
+#ifndef SCENE_SCENE_H_
+#define SCENE_SCENE_H_
+
+namespace scene {
+
+class Scene {
+
+};
+
+} // namespace
+
+#endif // SCENE_SCENE_H_
